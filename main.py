d4a1bbda1b4aa4296dfabc2117d94265457bd110
gabicaruso <gabriela.caruso@yahoo.com.br>
2020-09-11 23:10:21 +0000

    APS finalizada para conceito A

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, Dict
 from enum import Enum
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Body, Query
 from pydantic import BaseModel, Field
 import uuid
 
@@ -11,10 +11,10 @@ fake_db = {}
 
 class TaskIn(BaseModel):
     name: str = Field(
-        description="Name of the task (imput by user)",
+        description="Name of the task (user imput)",
     )
     description: str = Field(
-        description="Description of the task (imput by user)",
+        description="Description of the task (user imput)",
     )
 
 
@@ -48,13 +48,22 @@ class TaskStatusModel(str, Enum):
     done = "done"
     not_done = "not_done"
 
+
+@app.get("/",
+    summary="Root",
+    description="Used to check if the network is running",)
+
+def get_root():
+    return {"Check": "Running"}
+
+
 @app.post("/tasks",
     summary="Create Tasks",
     description="Used to create new tasks",
     response_description="New task",
     response_model=TaskOut)
 
-async def create_task(task: TaskIn):
+async def create_task(task: TaskIn = Body(...)):
 
     global fake_db
     id = uuid.uuid4()
@@ -65,7 +74,7 @@ async def create_task(task: TaskIn):
     return task_out_db
 
 
-@app.get("/tasks_list", 
+@app.get("/tasks_list/", 
     summary="Read Tasks",
     description="Used to consult the sistem in order to retrieve tasks on the dictionary",
     response_description="Dictionary containing all tasks based on the query parameter or returning all the tasks created if no query is sent",
@@ -95,29 +104,39 @@ async def read_task(q: Optional[TaskStatusModel] = Query(
         return fake_db
 
 
-@app.patch("/tasks/{task_id}",
+@app.patch("/task_update/{task_id}",
     summary="Update Tasks",
-    description="Used to update the description and the status of the tasks on the dictionary",
-    response_description="New task",
+    description="Used to update the description and the status of a task on the dictionary",
+    response_description="Dictionary containing the altered task",
     response_model=TaskOut)
-def update_item(task_id: uuid.UUID, task: TaskInUpdate):
+def update_item(*, task_id: uuid.UUID = Path(
+    ..., 
+    description="The ID of the task to be altered", 
+    example= "Example: 3fa85f64-5717-4562-b3fc-2c963f66afa6"), 
+    task: TaskInUpdate):
     
     global fake_db
 
     task_db = fake_db.get(task_id)
     update_data = task_db.dict()
-    #updated_task = task_db.copy(update = update_data)
     update_data.update(**task.dict())
     task_out = TaskOut(**update_data)
     fake_db.update({task_id: task_out})
 
     return task_out
 
-    #return task
 
-@app.delete("/tasks/{task_id}")
+@app.delete("/task_delete/{task_id}",
+    summary="Delete Tasks",
+    description="Used to delete a task on the dictionary",
+    response_description="Dictionary containing all the remaining tasks",
+    response_model=Dict[uuid.UUID, TaskOut])
 
-def delete_task(task_id: uuid.UUID):
+def delete_task(*,task_id: uuid.UUID = Path(
+    ..., 
+    description="The ID of the task to be deleted", 
+    example= "Example: 3fa85f64-5717-4562-b3fc-2c963f66afa6")):
+
     global fake_db
     task_db = fake_db.get(task_id)
     del fake_db[task_id]
