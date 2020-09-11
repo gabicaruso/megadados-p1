6e9735bfc3394104919c5e73a8127ffa85c11f2a
gabicaruso <gabriela.caruso@yahoo.com.br>
2020-09-11 19:22:25 +0000

    conceito B (conceito A quase finalizado)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from typing import Optional, List, Dict
 from enum import Enum
 from fastapi import FastAPI, Query
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 import uuid
 
 app = FastAPI()
@@ -11,68 +10,116 @@ fake_db = {}
 
 
 class TaskIn(BaseModel):
-    name: str
-    description: str
+    name: str = Field(
+        description="Name of the task (imput by user)",
+    )
+    description: str = Field(
+        description="Description of the task (imput by user)",
+    )
 
 
 class TaskOut(BaseModel):
-    task_id: uuid.UUID
-    name: str
-    description: str
-    status: bool
+    task_id: uuid.UUID = Field(
+        description="Id of the task (UUID)",
+    )
+    name: str = Field(
+        description="Name of the task",
+    )
+    description: str = Field(
+        description="Description of the task",
+    )
+    status: bool = Field(
+        description="Status of the task (done or not done yet)",
+        example="True, False"
+    )
+
+
+class TaskInUpdate(BaseModel):
+    description: str = Field(
+        description="Description of the task",
+    )
+    status: bool = Field(
+        description="Status of the task (done or not done yet)",
+        example="True, False"
+    )
 
 
 class TaskStatusModel(str, Enum):
     done = "done"
     not_done = "not_done"
 
+@app.post("/tasks",
+    summary="Create Tasks",
+    description="Used to create new tasks",
+    response_description="New task",
+    response_model=TaskOut)
 
-@app.get("/")
-def get_root():
-    return {"Hello": "World"}
-
-
-@app.post("/tasks", response_model=TaskOut)
 async def create_task(task: TaskIn):
+
     global fake_db
     id = uuid.uuid4()
+
     task_out_db = TaskOut(**task.dict(), status=False, task_id=id)
     fake_db.update({id: task_out_db})
+
     return task_out_db
 
 
-@app.get("/tasks_list", response_model=Dict[uuid.UUID, TaskOut])
-async def read_task(q: Optional[TaskStatusModel] = Query(None)):
+@app.get("/tasks_list", 
+    summary="Read Tasks",
+    description="Used to consult the sistem in order to retrieve tasks on the dictionary",
+    response_description="Dictionary containing all tasks based on the query parameter or returning all the tasks created if no query is sent",
+    response_model=Dict[uuid.UUID, TaskOut])
+
+async def read_task(q: Optional[TaskStatusModel] = Query(
+        None,
+        alias="status",
+        title="Query filter based on status",
+        description="Used to return only the tasks with the status in question or all of then if no status is sent",
+        example="Example: done, not_done")):
+
     global fake_db
     q_dict = {}
+
     if q != None:
         for task in fake_db.values():
+
             if task.status and q == TaskStatusModel.done:
                 q_dict.update({task.task_id: task})
+
             elif not task.status and q == TaskStatusModel.not_done:
                 q_dict.update({task.task_id: task})
+
         return q_dict
     else:
         return fake_db
 
 
-@app.put("/tasks/{task_id}")
-def update_item(task_id: uuid.UUID, task: TaskOut):
+@app.patch("/tasks/{task_id}",
+    summary="Update Tasks",
+    description="Used to update the description and the status of the tasks on the dictionary",
+    response_description="New task",
+    response_model=TaskOut)
+def update_item(task_id: uuid.UUID, task: TaskInUpdate):
+    
     global fake_db
 
-    # task_db = fake_db.get(task_id)
-    # fake_db.update({task_db.id: task})
+    task_db = fake_db.get(task_id)
+    update_data = task_db.dict()
+    #updated_task = task_db.copy(update = update_data)
+    update_data.update(**task.dict())
+    task_out = TaskOut(**update_data)
+    fake_db.update({task_id: task_out})
 
-    for tasks in list(fake_db.values()):
-        if tasks.task_id == task_id:
-            fake_db["name"] = task.name
-            fake_db["description"] = task.description
-            fake_db["status"] = task.status
-    #               fake_db.update({"taskid": tasks.taskid,
-    #               "name": tasks.name,
-    #               "description": tasks.description,
-    #               "status": tasks.status})
+    return task_out
 
-        print(fake_db.values())
+    #return task
 
-    return task
+@app.delete("/tasks/{task_id}")
+
+def delete_task(task_id: uuid.UUID):
+    global fake_db
+    task_db = fake_db.get(task_id)
+    del fake_db[task_id]
+
+    return fake_db
